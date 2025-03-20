9776b47f7bcb9c2ad5f09ab2a943e0230886f7f9
caio-lauro <caiolaurodelima@gmail.com>
2025-03-19 22:19:49 -0300

    feat: create print.h

---
+++ include/print.h
@@ -0,0 +1,9 @@
+#ifndef PRINT_HEADER
+#define PRINT_HEADER
+
+#include "bmp.h"
+#include "colors.h"
+
+void printAscii(RGBTRIPLE **image, char **asciiMatrice, int height, int width, int useColor);
+
+#endif
