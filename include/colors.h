2ba452bde6914b773a009e724402616af494753d
caio-lauro <caiolaurodelima@gmail.com>
2025-03-19 22:19:32 -0300

    feat: create colors.h

---
+++ include/colors.h
@@ -0,0 +1,13 @@
+#ifndef COLORS_HEADER
+#define COLORS_HEADER
+
+#define RED     "\033[0;31m"
+#define GREEN   "\033[0;32m"
+#define BLUE    "\033[0;34m"
+#define BLACK   "\033[0;30m"
+#define WHITE   "\033[0;37m"
+#define YELLOW  "\033[0;33m"
+#define PURPLE  "\033[0;35m"
+#define CYAN    "\033[0;36m"
+
+#endif
